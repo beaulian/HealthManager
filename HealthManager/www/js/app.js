9340185389a5fb2ad18db0b035eee6e5e6818c01
liu772992382 <772992382@qq.com>
2016-01-22 17:20:06 +0800

    index

--- HealthManager/www/js/app.js
+++ HealthManager/www/js/app.js
@@ -5,5 +5,37 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('todo', ['ionic'])
+angular.module('ionicApp', ['ionic'])
 
+.controller('MyCtrl', function($scope) {
+
+  $scope.data = {
+    showDelete: false
+  };
+
+  $scope.edit = function(item) {
+    alert('Edit Item: ' + item.id);
+  };
+  $scope.share = function(item) {
+    alert('Share Item: ' + item.id);
+  };
+
+  $scope.moveItem = function(item, fromIndex, toIndex) {
+    $scope.items.splice(fromIndex, 1);
+    $scope.items.splice(toIndex, 0, item);
+  };
+
+  $scope.onItemDelete = function(item) {
+    $scope.items.splice($scope.items.indexOf(item), 1);
+  };
+
+  $scope.items = [
+    { id: 0 ,imgPath:'img/adam.jpg'},
+    { id: 1 ,imgPath:'img/ben.png'},
+    { id: 2 ,imgPath:'img/ionic.png'},
+    { id: 3 ,imgPath:'img/max.png'},
+    { id: 4 ,imgPath:'img/mike.png'},
+    { id: 5 ,imgPath:'img/perry.png'}
+  ];
+
+});
