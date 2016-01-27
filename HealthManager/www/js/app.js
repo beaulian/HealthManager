c5954316593446c2d0ab3953d33582f524417a41
liu772992382 <772992382@qq.com>
2016-01-27 13:34:47 +0800

    nav1

--- HealthManager/www/js/app.js
+++ HealthManager/www/js/app.js
@@ -3,39 +3,113 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('ionicApp', ['ionic'])
 
-.controller('MyCtrl', function($scope) {
+  .config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
 
-  $scope.data = {
-    showDelete: false
-  };
+  	$ionicConfigProvider.platform.ios.tabs.style('standard');
+  	$ionicConfigProvider.platform.ios.tabs.position('bottom');
+  	$ionicConfigProvider.platform.android.tabs.style('standard');
+  	$ionicConfigProvider.platform.android.tabs.position('standard');
 
-  $scope.edit = function(item) {
-    alert('Edit Item: ' + item.id);
-  };
-  $scope.share = function(item) {
-    alert('Share Item: ' + item.id);
-  };
+  	$ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+  	$ionicConfigProvider.platform.android.navBar.alignTitle('left');
 
-  $scope.moveItem = function(item, fromIndex, toIndex) {
-    $scope.items.splice(fromIndex, 1);
-    $scope.items.splice(toIndex, 0, item);
-  };
+  	$ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+  	$ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
 
-  $scope.onItemDelete = function(item) {
-    $scope.items.splice($scope.items.indexOf(item), 1);
-  };
+  	$ionicConfigProvider.platform.ios.views.transition('ios');
+  	$ionicConfigProvider.platform.android.views.transition('android');
 
-  $scope.items = [
-    { id: 0 ,imgPath:'img/adam.jpg'},
-    { id: 1 ,imgPath:'img/ben.png'},
-    { id: 2 ,imgPath:'img/ionic.png'},
-    { id: 3 ,imgPath:'img/max.png'},
-    { id: 4 ,imgPath:'img/mike.png'},
-    { id: 5 ,imgPath:'img/perry.png'}
-  ];
+    $stateProvider
+      .state('tabs', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+      })
+      .state('tabs.home', {
+        url: "/home",
+        views: {
+          'home-tab': {
+            templateUrl: "templates/home.html",
+            controller: 'HomeTabCtrl'
+          }
+        }
+      })
+      .state('tabs.facts', {
+        url: "/facts",
+        views: {
+          'home-tab': {
+            templateUrl: "templates/facts.html"
+          }
+        }
+      })
+      .state('tabs.facts2', {
+        url: "/facts2",
+        views: {
+          'home-tab': {
+            templateUrl: "templates/facts2.html"
+          }
+        }
+      })
+      .state('tabs.about', {
+        url: "/about",
+        views: {
+          'about-tab': {
+            templateUrl: "templates/about.html"
+          }
+        }
+      })
+      .state('tabs.navstack', {
+        url: "/navstack",
+        views: {
+          'about-tab': {
+            templateUrl: "templates/nav-stack.html"
+          }
+        }
+      })
+      .state('tabs.contact', {
+        url: "/contact",
+        views: {
+          'contact-tab': {
+            templateUrl: "templates/contact.html"
+          }
+        }
+      });
 
-});
+
+     $urlRouterProvider.otherwise("/tab/home");
+
+  })
+
+  .controller('HomeTabCtrl', function($scope) {
+    console.log('HomeTabCtrl');
+      $scope.data = {
+        showDelete: false
+      };
+
+      $scope.edit = function(item) {
+        alert('Edit Item: ' + item.id);
+      };
+      $scope.share = function(item) {
+        alert('Share Item: ' + item.id);
+      };
+
+      $scope.moveItem = function(item, fromIndex, toIndex) {
+        $scope.items.splice(fromIndex, 1);
+        $scope.items.splice(toIndex, 0, item);
+      };
+
+      $scope.onItemDelete = function(item) {
+        $scope.items.splice($scope.items.indexOf(item), 1);
+      };
+
+      $scope.items = [
+        { id: 0 ,imgPath:'img/adam.jpg'},
+        { id: 1 ,imgPath:'img/ben.png'},
+        { id: 2 ,imgPath:'img/ionic.png'},
+        { id: 3 ,imgPath:'img/max.png'},
+        { id: 4 ,imgPath:'img/mike.png'},
+        { id: 5 ,imgPath:'img/perry.png'}
+      ];
+  });
