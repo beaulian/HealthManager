10175cb928a6efad09009fc8335887e7c8d5be83
shenaishiren <gjw.jesus@qq.com>
2016-03-12 16:18:48 +0800

    改用celery消息队列发邮件

--- server/mail.py
+++ server/mail.py
@@ -2,25 +2,27 @@
 
 import smtplib
 from config import *
+from celery import Celery
 from email.mime.text import MIMEText
 
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+celery = Celery('healthmanager', broker='redis://localhost:6379/4')
 
+
+@celery.task
 def send_email(fuction, mail_receiver_list, content):
     msg = MIMEText(content,_subtype="html",_charset="utf-8")
     msg["Subject"] = fuction
     msg["From"] = APP_NAME + "<" + MAIL_SENDER + ">"
     msg["To"] = ";".join(mail_receiver_list)
-    try:
-        server = smtplib.SMTP()
-        server.connect(MAIL_HOST)
-        server.starttls()
-        server.login(MAIL_SENDER, MAIL_PASSWORD)
-        server.sendmail(MAIL_SENDER,mail_receiver_list,msg.as_string())
-        server.close()
-    except Exception:
-        return False
-    return True
+    
+    server = smtplib.SMTP()
+    server.connect(MAIL_HOST)
+    server.starttls()
+    server.login(MAIL_SENDER, MAIL_PASSWORD)
+    server.sendmail(MAIL_SENDER,mail_receiver_list,msg.as_string())
+    server.close()
+    
