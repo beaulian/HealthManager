8b2827b736a78ee4bc2ff0426f41dd69259896f8
shenaishiren <gjw.jesus@qq.com>
2016-04-10 18:31:52 +0800

    update

--- HealthManager/platforms/android/assets/www/js/controllers.js
+++ HealthManager/platforms/android/assets/www/js/controllers.js
@@ -240,12 +240,12 @@ angular.module('starter.controllers', ['ngCookies'])
 		var now             = new Date().getTime(),
     _5_sec_from_now = new Date(now + 5*1000);
 
-	cordova.plugins.notification.local.schedule({
-	    text: "Delayed Notification",
-	    at: _5_sec_from_now,
-	    led: "FF0000",
-	    sound: null
-	});
+		cordova.plugins.notification.local.schedule({
+		    text: "Delayed Notification",
+		    at: _5_sec_from_now,
+		    led: "FF0000",
+		    sound: null
+		});
 	};
 
 
